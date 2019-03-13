229b4e7976daebcd16a9e8a45b6e2560a8a0ca77
zhang liang <zhangliang@usechain.net>
2019-03-13 10:56:04 +0800

    revert to solidity version to 0.4.20

--- contracts/minerlist/minerlist.sol
+++ contracts/minerlist/minerlist.sol
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-usechain library. If not, see <http://www.gnu.org/licenses/>.
 
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.20;
 
 contract MinerList {
 

cbb3e4a51cd6c2a5883167af8bd40e4e8e76d21d
zhang liang <zhangliang@usechain.net>
2019-03-14 10:14:34 +0800

    update env.sh for detect go version

--- build/env.sh
+++ build/env.sh
@@ -7,6 +7,41 @@ if [ ! -f "build/env.sh" ]; then
     exit 2
 fi
 
+#!/bin/sh
+checkGoVersion()
+{
+    #Recommended Version 1.10.1
+    V1=1
+    V2=10
+    V3=1
+
+    echo The minimum golang version required is: $V1.$V2.$V3
+
+    #Get golang current version
+    CV1=`go version 2>&1|awk '{print $3}'|awk -F '.' '{print $1}'|awk -F, '{print substr($1,length($1)-0)}'`
+    CV2=`go version 2>&1|awk '{print $3}'|awk -F '.' '{print $2}'`
+    CV3=`go version 2>&1|awk '{print $3}'|awk -F '.' '{print $3}'`
+
+    echo Your current golang version is : $CV1.$CV2.$CV3
+
+    if [ $CV1 -lt $V1 ];then
+        echo 'Please update to version 1.10 or higher'
+        exit 1
+    elif [ $CV1 -eq $V1 ];then
+        if [ $CV2 -lt $V2 ];then
+            echo 'Please update to version 1.10 or higher'
+            exit 1
+        elif [ $CV2 -eq $V2 ];then
+            if [ $CV3 -lt $V3 ];then
+                echo 'Please update to version 1.10 or higher'
+                exit 1
+            fi
+        fi
+    fi
+}
+checkGoVersion
+echo Your golang version is OK!
+
 # Create fake Go workspace if it doesn't exist yet.
 workspace="$PWD/build/_workspace"
 root="$PWD"

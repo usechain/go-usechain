2595e660b2b9bd6f69ea6fd46e09485d64f11555
zhang liang <zhangliang@usechain.net>
2019-03-20 20:26:15 +0800

    add new bootnode for moonet

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -29,6 +29,8 @@ var TestnetBootnodes = []string{}
 var MoonetBootnodes = []string{
 	// Usechain Go moonet Bootnodes
 	"enode://1de6d73d3b7a288de8f571b8b48a89970053f26beeba787c9f36044cb2d1e89c0c94e802e579798efcc39c48a5e23a160e1ab9ae8f0b752725c7ab8ea40b3194@119.23.41.121:40404",
+	"enode://f7fee44270a73ab44762598c0f7f2e0fc083d6eee5cbfbc365bd053d8c04886235c3a21fc4e5f3fb2a1deb257612e23c232b090bc2bf650c56fe839625d26cfa@47.112.117.48:40404",
+	"enode://8613e162784e292803196f4a7c04c5ae0cfbb760acd3f1fa5a63222dcc0bc9d6450f8f9c113db622dcad3885849a9c9d9f23424bc94a6f15deb2643c6124a856@39.97.174.114:40404",
 }
 
 // DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the

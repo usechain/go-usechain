febede4f31673a0bd814c5e6b105063487759a1d
ningxin18 <292409083@qq.com>
2019-05-10 15:25:57 +0800

    add mainAccount verify addr

--- contracts/credit/newContract.sol
+++ contracts/credit/newContract.sol
@@ -1,97 +1,12 @@
 pragma solidity ^0.4.20;
 
-//import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/access/roles/SignerRole.sol";
-
-/**
- * @title Roles
- * @dev Library for managing addresses assigned to a Role.
- */
-library Roles {
-    struct Role {
-        mapping (address => bool) bearer;
-    }
-
-    /**
-     * @dev give an account access to this role
-     */
-    function add(Role storage role, address account) internal {
-        require(account != address(0));
-        require(!has(role, account));
-
-        role.bearer[account] = true;
-    }
-
-    /**
-     * @dev remove an account's access to this role
-     */
-    function remove(Role storage role, address account) internal {
-        require(account != address(0));
-        require(has(role, account));
-
-        role.bearer[account] = false;
-    }
-
-    /**
-     * @dev check if an account has this role
-     * @return bool
-     */
-    function has(Role storage role, address account) internal view returns (bool) {
-        require(account != address(0));
-        return role.bearer[account];
-    }
-}
-
-contract SignerRole {
-    using Roles for Roles.Role;
-
-    event SignerAdded(address indexed account);
-    event SignerRemoved(address indexed account);
-
-    Roles.Role private _signers;
-
-    function SignerRole() internal {
-        _addSigner(msg.sender);
-    }
-
-    modifier onlySigner() {
-        require(isSigner(msg.sender));
-        _;
-    }
-
-    function isSigner(address account) public view returns (bool) {
-        return _signers.has(account);
-    }
-
-    function addSigner(address account) public onlySigner {
-        _addSigner(account);
-    }
-
-    function renounceSigner() public {
-        _removeSigner(msg.sender);
-    }
-
-    function _addSigner(address account) internal {
-        _signers.add(account);
-        SignerAdded(account);
-    }
-
-    function _removeSigner(address account) internal {
-        _signers.remove(account);
-        SignerRemoved(account);
-    }
-}
-
-
 contract Committee {
     /// @notice Whether the msg.sender is committee or not.
     /// @return Whether the transfer was successful or not
     function IsOndutyCommittee(address _user) public view returns(bool);
 }
 
-contract CreditSystem is SignerRole{
-// contract CreditSystem is SignerRole{
-    event NewUserRegister(address indexed addr, bytes32 indexed hash);
-    event NewIdentity(address indexed addr, bytes32 indexed hash);
+contract CreditSystem {
     uint public unConfirmedMainAddressLen = 0;
     uint public unConfirmedSubAddressLen  = 0;
     uint public unEncryptedSubAddressLen = 0;
@@ -179,7 +94,10 @@ contract CreditSystem is SignerRole{
         RegisterIDtoAddr[_registerID].toAddress = msg.sender;
         RegisterID += 1;
         if (_ciphertext == true) {
-               MainAccount[msg.sender].status = 3;
+                MainAccount[msg.sender].status = 3;
+                RegisterIDtoAddr[_registerID].verified = true;
+                confirmedMainAddress.push(msg.sender);
+                confirmedMainAddressLen = confirmedMainAddress.length;
                return true;
         }
         MainAccount[msg.sender].status = 1;
@@ -245,12 +163,13 @@ contract CreditSystem is SignerRole{
         return UnEncryptedSubAddrID.length;
     }
 
-    function verifyHash(uint _registerID, bytes32 _hash, uint _status)
+    function verifyHash(uint _registerID, bytes32 _hash, uint _status, address _verifiedAddr)
         public
         onlyCommittee(msg.sender)
         UnVerifiedRegisterID(_registerID)
         returns(bool){
             address _addr = RegisterIDtoAddr[_registerID].toAddress;
+            require(_verifiedAddr == _addr);
             require(MainAccount[_addr].addr != address(0));
             require(MainAccount[_addr].status == 1);
             require(MainAccount[_addr].hashKey == _hash);

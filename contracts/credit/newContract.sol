1fcc0a1e1b499795068b364361552e3e9a709b56
ningxin18 <292409083@qq.com>
2019-05-13 14:28:35 +0800

    credit contract verifyHash

--- contracts/credit/newContract.sol
+++ contracts/credit/newContract.sol
@@ -47,8 +47,13 @@ contract CreditSystem {
         address toAddress;
     }
 
-    mapping (uint => registeredAddr) public RegisterIDtoAddr;
+    struct hashKey {
+        address toAddress;
+        uint status;
+    }
 
+    mapping (uint => registeredAddr) public RegisterIDtoAddr;
+    mapping (bytes32 => hashKey) public HashKeyToAddr;
 
     /// @notice only committee can do
     modifier onlyCommittee(address _user) {
@@ -61,12 +66,12 @@ contract CreditSystem {
     }
 
     modifier MainAccountUnVerifying(address _addr) {
-        require (MainAccount[_addr].status != 1); // verifying status cannot register, unregistered or rejected can register, verified can update info.
+        require (MainAccount[_addr].status != 1); // Verifying status cannot register, unregistered or rejected can register, verified can update info.
         _;
     }
 
     modifier SubAccountUnVerifying(address _addr) {
-        require (SubAccount[_addr].status != 1); // same annotation as above
+        require (SubAccount[_addr].status != 1); // Same annotation as above
         _;
     }
 
@@ -75,7 +80,7 @@ contract CreditSystem {
         _;
     }
 
-    // main account register
+    // Main account register
     function register(string memory _publicKey,
                     bytes32 _hashKey,
                     bytes memory _identity,
@@ -85,6 +90,7 @@ contract CreditSystem {
         public
         payable
         returns(bool){
+        require(_ciphertext==false);
         MainAccount[msg.sender].addr = msg.sender;
         MainAccount[msg.sender].publicKey = _publicKey;
         MainAccount[msg.sender].hashKey = _hashKey;
@@ -163,25 +169,33 @@ contract CreditSystem {
         return UnEncryptedSubAddrID.length;
     }
 
-    function verifyHash(uint _registerID, bytes32 _hash, uint _status, address _verifiedAddr)
+    function verifyHash(uint _registerID, bytes32 _hashKey, uint _status, address _verifiedAddr)
         public
         onlyCommittee(msg.sender)
         UnVerifiedRegisterID(_registerID)
         returns(bool){
-            address _addr = RegisterIDtoAddr[_registerID].toAddress;
-            require(_verifiedAddr == _addr);
-            require(MainAccount[_addr].addr != address(0));
-            require(MainAccount[_addr].status == 1);
-            require(MainAccount[_addr].hashKey == _hash);
-            if (SubAccount[_addr].status != 0 ) { // sub account can't verified as main account
+            address _IdToAddr = RegisterIDtoAddr[_registerID].toAddress;
+            address _hashkeyToAddr = HashKeyToAddr[_hashKey].toAddress;
+            require(_verifiedAddr == _IdToAddr);
+            require(MainAccount[_IdToAddr].addr != address(0));
+            require(MainAccount[_IdToAddr].status == 1);
+            require(MainAccount[_IdToAddr].hashKey == _hashKey);
+            if (SubAccount[_IdToAddr].status != 0 ) { // sub account can't verified as main account
                 _status = 0;    // adjust mainAccount to unregister status in order to getAccountStatus() can return correct value
             }
-            MainAccount[_addr].status = _status;
+            MainAccount[_IdToAddr].status = _status;
             if (_status == 3) {
                 RegisterIDtoAddr[_registerID].verified = true;
-                confirmedMainAddress.push(_addr);
+                confirmedMainAddress.push(_IdToAddr);
                 confirmedMainAddressLen = confirmedMainAddress.length;
+                if (_hashkeyToAddr != _IdToAddr && MainAccount[_hashkeyToAddr].status ==3) {
+                    MainAccount[_hashkeyToAddr].status = 4;
+                }
             }
+
+            HashKeyToAddr[_hashKey].toAddress = _IdToAddr;
+            HashKeyToAddr[_hashKey].status = _status;
+
             for (uint i= 0; i < unConfirmedMainAddressLen; i++) {
                 if (UnConfirmedMainAddrID[i] == _registerID) {
                     UnConfirmedMainAddrID[i] = UnConfirmedMainAddrID[unConfirmedMainAddressLen - 1];

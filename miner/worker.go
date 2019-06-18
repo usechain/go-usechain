70172a82bf0a5a2250b432a53ad215a60a8ee59c
lemengbin <lemengbin@163.com>
2019-06-18 16:19:51 +0800

    fill all same hashes in payload of pbft txns into pending when current block is a checkpoint

--- miner/worker.go
+++ miner/worker.go
@@ -509,8 +509,10 @@ func (self *worker) commitNewWork() {
 	committeeCnt := self.chain.GetCommitteeCount()
 	var pending map[common.Address]types.Transactions
 	if header.IsCheckPoint.Cmp(common.Big1) == 0 && atomic.LoadInt32(&self.mining) == 1 {
-		pending, err = self.eth.TxPool().GetValidPbft(blockNumber.Uint64()-1, core.GetIndexForVote(time.Now().Unix(), parent.Time().Int64()))
-		gen, targetHash, _ := canGenBlockInCheckPoint(pending, committeeCnt)
+		allPbftTxs, _ := self.eth.TxPool().GetValidPbft(blockNumber.Uint64()-1, core.GetIndexForVote(time.Now().Unix(), parent.Time().Int64()))
+		gen := false
+		targetHash := common.Hash{}
+		pending, gen, targetHash, _ = canGenBlockInCheckPoint(allPbftTxs, committeeCnt)
 		if !gen {
 		DONE2:
 			for {
@@ -667,9 +669,9 @@ func (self *worker) isMiner(totalMinerNum *big.Int, blockNumber *big.Int) bool {
 	return true
 }
 
-func canGenBlockInCheckPoint(txs map[common.Address]types.Transactions, cnt int32) (bool, common.Hash, uint32) {
+func canGenBlockInCheckPoint(txs map[common.Address]types.Transactions, cnt int32) (map[common.Address]types.Transactions, bool, common.Hash, uint32) {
 	if int32(len(txs)) < (cnt+1)/common.VoteThreshold {
-		return false, common.Hash{}, 0
+		return nil, false, common.Hash{}, 0
 	}
 
 	hashCount := make(map[common.Hash]uint32)
@@ -696,10 +698,27 @@ func canGenBlockInCheckPoint(txs map[common.Address]types.Transactions, cnt int3
 	}
 
 	if int32(maxCount) < (cnt+1)/common.VoteThreshold {
-		return false, maxHash, maxCount
+		return nil, false, maxHash, maxCount
 	}
 
-	return true, maxHash, maxCount
+	// get all txs are same with maxHash
+	pending := make(map[common.Address]types.Transactions)
+	for addr, list := range txs {
+		tempPendingTxs := make(types.Transactions, 0, list.Len())
+		for i := 0; i < list.Len(); i++ {
+			payload := list[i].Data()
+			hash := common.BytesToHash(payload[:common.HashLength])
+			if hash != maxHash {
+				continue
+			}
+			tempPendingTxs = append(tempPendingTxs, list[i])
+		}
+
+		if tempPendingTxs.Len() > 0 {
+			pending[addr] = tempPendingTxs
+		}
+	}
+	return pending, true, maxHash, maxCount
 }
 
 func (self *worker) commitUncle(work *Work, uncle *types.Header) error {
